1e334a1e41ec09e90416caac124cd7f0c1eed907
furetur <furetur@gmail.com>
2021-07-06 19:10:29 +0500

    factorial example now uses factorial built in function

--- src/main/kotlin/examples/Fact6.kt
+++ src/main/kotlin/examples/Fact6.kt
@@ -2,31 +2,14 @@ package examples
 
 import codegen.BytecodeBuilder
 import codegen.dsl.*
+import stdlib.FactFunction
+import stdlib.addBuiltInFunction
 import java.io.File
 
 fun main() {
     val builder = BytecodeBuilder()
 
-    val factFunction = builder.addFunction("fact", 1) {
-        ifStatement(
-            condition = {
-                equal(left = { getLocalVariable(1) }, right = { literal(1) })
-            },
-            thenCode = {
-                ret { literal(1) }
-            }
-        )
-        ret {
-            multiply(
-                left = { getLocalVariable(1) },
-                right = {
-                    self()
-                    subtract(left = { getLocalVariable(1) }, right = { literal(1) })
-                    call(1)
-                }
-            )
-        }
-    }
+    val factFunction = builder.addBuiltInFunction(FactFunction)
 
     builder.main {
         print {
@@ -39,4 +22,5 @@ fun main() {
     val file = File("output/fact_6.bytecode")
     file.createNewFile()
     builder.bytecode().write(file)
+    builder.printAssembly()
 }
